3061b211350f1c977a7333dbaf658179115a4629
0xYeah <wmyeah017@gmail.com>
2026-07-02 12:43:01 +0800

    Release:--: v0.0.20_2026-07-02_04:43:01_UTC

--- config/config.go
+++ config/config.go
@@ -2,7 +2,7 @@ package config
 
 const (
 	ProjectName         = "fltk2go"
-	ProjectVersion      = "v0.0.19"
+	ProjectVersion      = "v0.0.20"
 	FLTKPreBuildVersion = "release-1.4.4"
 	FLTKCppStandard     = "c++11"
 )
